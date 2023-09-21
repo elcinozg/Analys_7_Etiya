f42fe9038ccb1c5d343bdd9c56641c43082a04df
elcinozg <145574333+elcinozg@users.noreply.github.com>
2023-09-21 15:06:18 +0300

    Update grades.py

--- grades.py
+++ grades.py
@@ -4,40 +4,50 @@ final = int(input("Final notunuzu giriniz: "))
 
  
 
-average = midterm*0.4 + final*0.6
-
  
 
-if(average <= 49):
+if(final < 40 or (midterm == final*2)):
 
-    print("Ortalamanız: ", average, "Harf notunuz: FF")
-
- 
-
-elif(average >= 50 and average <= 59):
-
-    print("Ortalamanız: ", average, "Harf notunuz: DD")
-
- 
-
-elif(average >= 60 and average <= 69):
-
-    print("Ortalamanız: ", average, "Harf notunuz: CC")
-
- 
-
-elif(average >= 70 and average <= 79):
-
-    print("Ortalamanız: ", average, "Harf notunuz: BB")
-
- 
-
-elif(average >= 80 and average <= 100):
-
-    print("Ortalamanız: ", average, "Harf notunuz: AA")
+    print("Kaldınız.. ")
 
  
 
 else:
 
-    print("Hatalı giriş yaptınız.. ")
+    average = midterm*0.4 + final*0.6
+
+ 
+
+    if(average <= 49):
+
+        print("Ortalamanız: ", average, "Harf notunuz: FF, Kaldınız.. ")
+
+ 
+
+    elif(average >= 50 and average <= 59):
+
+        print("Ortalamanız: ", average, "Harf notunuz: DD, Geçtiniz..")
+
+ 
+
+    elif(average >= 60 and average <= 69):
+
+        print("Ortalamanız: ", average, "Harf notunuz: CC, Geçtiniz..")
+
+ 
+
+    elif(average >= 70 and average <= 79):
+
+        print("Ortalamanız: ", average, "Harf notunuz: BB, Geçtiniz..")
+
+ 
+
+    elif(average >= 80 and average <= 100):
+
+        print("Ortalamanız: ", average, "Harf notunuz: AA, Geçtiniz..")
+
+ 
+
+    else:
+
+        print("Hatalı giriş yaptınız.. ")
