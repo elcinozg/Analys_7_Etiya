b72be3ad12392104e582017e536997886e88cd23
elcinozg <145574333+elcinozg@users.noreply.github.com>
2023-09-22 16:52:29 +0300

    yeni

--- school/school.py
+++ school/school.py
@@ -2,4 +2,8 @@ from student import student
 from teacher import teacher
 
 
-print("aa")
+class school(student,teacher):
+    def __init__(self,name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
